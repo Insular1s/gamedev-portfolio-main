e434cdb27e20460a015093b7c03d18a04900a76d
Insular1s <97142204+Insular1s@users.noreply.github.com>
2024-11-26 11:03:55 +0900

    updated front page

--- src/views/About.vue
+++ src/views/About.vue
@@ -12,7 +12,7 @@
 
       <div style="margin-top: 20px;">I've worked on <router-link to="/other-projects">multiple research, </router-link> <router-link to="/game-projects">2D and 3D Game Projects </router-link> (to showcase my capabilities using Unity, or just to play around), on <router-link to="/other-projects">mobile and web app development</router-link>, and enjoyed modeling and printing them on 3D Printer as well.</div>
 
-      <div style="margin-top: 40px;">I'm <strong>currently looking for a job</strong> as a <strong>Mixed Reality (XR)</strong> game and contents creator. You can reach me at <a href="mailto:aulia91111@gmail.com">aulia91111@gmail.com</a> or <router-link to="/contact">through here</router-link>.</div>
+      <div style="margin-top: 40px;">I'm <strong>currently looking for a job</strong> as a <strong>Game Programmer</strong>and other game contents creator related job. You can reach me at <a href="mailto:aulia91111@gmail.com">aulia91111@gmail.com</a> or <router-link to="/contact">through here</router-link>.</div>
     </div>
 
     <div class="photo">
