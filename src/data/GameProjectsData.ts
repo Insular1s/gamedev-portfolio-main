1d5326b50459813ac01e62c08881995ce0eacb4d
Insular1s <97142204+Insular1s@users.noreply.github.com>
2024-12-05 17:07:36 +0900

    updated game project details

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -9,7 +9,11 @@ export default [
     </div>
 
     <div class="paragraph center">
-        <a href="https://bedroom-studio.itch.io/metalventure" target="_blank"><img src="img/projects/itchio_logo.svg" alt="Itch-io badge" /></a>
+        <video width="500px" 
+           height="400px" 
+           controls="controls">
+            <source src="vid/metalventure1.mp4" type="video/mp4" />
+        </video>
     </div>
 
     <div class="paragraph">
@@ -20,7 +24,7 @@ export default [
     </div>
 
     <div class="paragraph">
-        Main features :
+        <strong>Main Features :</strong>
         <ul>
         <li>A complete and easy to use framework to build your own 2D games</li>
         <li>Using Metroid-vania mechanics to enhance playability</li>
@@ -29,30 +33,73 @@ export default [
         </ul>
     </div>
 
-    <div class="paragraph center">
-        <img class="pc-screenshot" src="img/projects/metalventure-1.png" alt="Metalventure Screenshot" />
-        <img class="pc-screenshot" src="img/projects/metalventure-2.png" alt="Metalventure Screenshot" />
-    </div>
-    `, "#e80fb7", false, true),
-    new ProjectData("project-2", "Zishaku", "img/projects/zishaku-logo.png", `
     <div class="paragraph">
-        <strong>Zishaku</strong> is a game that I'm proud of to show my game design skill. I developed this game with a team of 5 in 2022 Global Game Jam. Zishaku is about an uncomplete robot that has to travel through space in order to collect his parts.
-        <br/>This game really shows how a simple game design can leads to interesting mechanics.
+        <strong>Development Tools :</strong>
+        <ul>
+        <li>Unity Game Engine</li>
+        <li>Audacity</li>
+        <li>Aseprite</li>
+        <li>Visual Studio Code</li>
+        </ul>
+    </div>
+
+    <div class="paragraph">
+        <strong>Game Controls :</strong>
+        <ul>
+        <li>W, A, S, D - Move Character</li>
+        <li>Left Click - Shoot</li>
+        <li>Right Click - Dash</li>
+        <li>Space - Jump</li>
+        <li>Space 2x - Double Jump</li>
+        <li>Hold S - Turn Into Ball Mode</li>
+        <li>When in Ball Mode :</li>
+        <li>Hold W - Standing Mode</li>
+        <li>WLeft Click - Place Mine</li>
+        </ul>
+    </div>
+
+    <div class="paragraph">
+        <strong>Job List :</strong>
+        <ul>
+        <li>Game Planning by : Myself</li>
+        <li>Game Programming by : Myself</li>
+        <li>Map and Level Design by : Myself</li>
+        <li>UI/UX Design by : Myself</li>
+        <li>Game Audios by : Myself</li>
+        <li>Game Assets by : <a href="https://itch.io/queue/c/463790/warped-packs?game_id=157789" target="_blank">Ansimuz (Warped Cave Asset)</a></li>
+        </ul>
     </div>
 
     <div class="paragraph center">
-        <a href="https://sols-atelier.itch.io/zishoku" target="_blank"><img src="img/projects/itchio_logo.svg" alt="Itch-io badge" /></a>
+        <img class="pc-screenshot" src="img/projects/metalventure-1.png" alt="Metalventure Screenshot" />
+        <img class="pc-screenshot" src="img/projects/metalventure-2.png" alt="Metalventure Screenshot" />
+        <img class="pc-screenshot" src="img/projects/metalventure-3.png" alt="Metalventure Screenshot" />
+        <img class="pc-screenshot" src="img/projects/project-1-icon.png" alt="Metalventure Screenshot" />
+    </div>
+    `, "#e80fb7", true, true),
+    new ProjectData("project-2", "Zishaku", "img/projects/zishaku-logo.png", `
+    <div class="paragraph">
+        <strong>Zishaku</strong> is a game that I'm proud of to show my game design skill. I developed this game with a team of 5 in 2022 Global Game Jam. 
+        <br/>This game is 2D puzzle game where you use magnets to push and pull Zishaku's parts around. Please enjoy! Made for the Global Game Jam 2022 with the theme of "duality".
+    </div>
+
+    <div class="paragraph center">
+        <video width="500px" 
+           height="400px" 
+           controls="controls">
+            <source src="vid/metalventure1.mp4" type="video/mp4" />
+        </video>
     </div>
 
     <div class="paragraph">
     <div class="notice">
         Windows build available on <a class="download-link" href="d/Zishoku 1.0.zip">Here</a>.
-        Source code is available on <a href="https://github.com/Solomar/GGJ2022" target="_blank">Github</a> or <a class="download-link" href="d/GGJ2022_Zishaku-main.zip">Here</a>.
+        Source code is available on <a class="download-link" href="d/GGJ2022_Zishaku-main.zip">Here</a>.
     </div>
     </div>
 
     <div class="paragraph">
-        Main features :
+        <strong>Main features :</strong>
         <ul>
         <li>This game uses a simple mechanic of a magnet that can push and pull stuffs.</li>
         <li>Challenging levels to test your gaming skill.</li>
@@ -61,11 +108,42 @@ export default [
         </ul>
     </div>
 
+    <div class="paragraph">
+        <strong>Development Tools :</strong>
+        <ul>
+        <li>Unity Game Engine</li>
+        <li>Audacity</li>
+        <li>Adobe Illustrator</li>
+        <li>Visual Studio Code</li>
+        </ul>
+    </div>
+
+    <div class="paragraph">
+        <strong>Game Controls :</strong>
+        <ul>
+        <li>Left Click - Put Magnet</li>
+        <li>Hold Left Click at Magnet - Rotate Magnet</li>
+        <li>Double Left Click at Magnet - Change Magnet Polarity</li>
+        <li>Space - Reset Level</li>
+        </ul>
+    </div>
+
+    <div class="paragraph">
+        <strong>Job List :</strong>
+        <ul>
+        <li>Game Design by : <a href="https://auliarizky.netlify.app/#/resume" target="_blank">Myself</a></li>
+        <li>Game Programming by : <a href="https://globalgamejam.org/users/batmania" target="_blank">Stephen</a></li>
+        <li>Game Programming by : Philippe</li>
+        <li>UI/UX Design by : <a href="https://globalgamejam.org/users/tomonky" target="_blank">Tomomi</a></li>
+        <li>Game Illustration by : <a href="https://globalgamejam.org/users/victoria-andino" target="_blank">Vicky</a></li>
+        </ul>
+    </div>
+
     <div class="paragraph center">
     <img class="pc-screenshot" src="img/projects/zishaku-1.png" alt="Zishaku Screenshot" />
     <img class="pc-screenshot" src="img/projects/zishaku-3.png" alt="Zishaku Screenshot" />
     </div>
-    `, "#5a78af"),
+    `, "#5a78af", true, false),
     new ProjectData("project-3", "Last Train Home", "img/projects/last-train-home-logo.png", `
     <div class="paragraph">
         <strong>Last Train Home</strong> is my VR Horror Game project that is still under development. The concept for this game is a psychological horror where no jumpscares are added in the game.
@@ -73,7 +151,11 @@ export default [
     </div>
 
     <div class="paragraph center">
-        <a href="https://github.com/Insular1s/HorrorProject" target="_blank"><img src="img/projects/github-logo.png" alt="Github badge" /></a>
+        <video width="500px" 
+           height="400px" 
+           controls="controls">
+            <source src="vid/metalventure1.mp4" type="video/mp4" />
+        </video>
     </div>
 
     <div class="paragraph">
@@ -83,7 +165,7 @@ export default [
     </div>
 
     <div class="paragraph">
-        Main features :
+        <strong>Main features :</strong>
         <ul>
         <li>Psychological Horror game that doesn't rely on cheap jumpscares</li>
         <li>The mechanics in this game inspired by <strong>Silent Hill P.T</strong>, where the players going on and on in infinite corridor like.</li>
@@ -93,17 +175,45 @@ export default [
         </ul>
     </div>
 
+    <div class="paragraph">
+        <strong>Development Tools :</strong>
+        <ul>
+        <li>Unreal Engine 5</li>
+        <li>Audacity</li>
+        <li>Blender & Maya</li>
+        <li>Visual Studio</li>
+        </ul>
+    </div>
+
+    <div class="paragraph">
+        <strong>Assets Used :</strong>
+        <ul>
+        <li><a href="https://www.unrealengine.com/marketplace/ja/product/horror-engine" target="_blank">Horror Engine</a></li>
+        <li><a href="https://www.fab.com/listings/39abdcd9-baa1-49a0-bb9b-90808d483bca" target="_blank">City Subway Train Modular</a></li>
+        </ul>
+    </div>
+
+    <div class="paragraph">
+        <strong>Job List :</strong>
+        <ul>
+        <li>Game Planning by : Myself</li>
+        <li>Game Programming by : Myself</li>
+        <li>Map and Level Design by : Myself</li>
+        <li>UI/UX Design by : Myself</li>
+        </ul>
+    </div>
+
     <div class="paragraph center">
         <img class="pc-screenshot" src="img/projects/last-train-home-1.png" alt="Last Train Home Screenshot" />
         <img class="pc-screenshot" src="img/projects/last-train-home-2.png" alt="Last Train Home Screenshot" />
         <img class="pc-screenshot" src="img/projects/last-train-home-3.png" alt="Last Train Home Screenshot" />
         <img class="pc-screenshot" src="img/projects/last-train-home-4.png" alt="Last Train Home Screenshot" />
     </div>
-    `, "#383838"),
-    new ProjectData("project-4", "Twilight Memoria", "img/projects/twilight-1.png", `
+    `, "#383838", true, false),
+    new ProjectData("project-4", "Twilight Memoria", "vid/twilight.gif", `
     <div class="paragraph">
     <strong>Twilight Memoria</strong> is a JRP game that me and my friend developed during our university years.
-    <br/>It really took us to another level, developing game with our limited time and resources.
+    <br/>It really took us to another level, developing game with our limited time and resources. I can't say much here but if you're interested please support us by buying the game and enjoy playing :)
     </div>
 
     <div class="paragraph center">
@@ -117,7 +227,7 @@ export default [
     </div>
     
     <div class="paragraph">
-        Main features :
+        <strong>Main features :</strong>
         <ul>
         <li>Unique battle system, where the player can throw a combo at the enemies</li>
         <li>First time battle might be difficult but as you progress the game it will be easier</li>
@@ -132,5 +242,5 @@ export default [
         <img class="pc-screenshot" src="img/projects/twilight-3.png" alt="Twilight Memoria Screenshot" />
         <img class="pc-screenshot" src="img/projects/twilight-4.png" alt="Twilight Memoria Screenshot" />
     </div>
-    `, "#e48246", false, true)
+    `, "#e48246", true, true)
 ];
